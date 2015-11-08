7fc9e0a900787c3b34d71fe20133f4974ad9c38f
Anatoly Volohov <proxwian@gmail.com>
2015-11-08 13:41:03 -0800

    Hello
    This is my first git on GitHUB

---
+++ src/ru/mcreation/proxwian/helloworld/Main.java
@@ -0,0 +1,13 @@
+package ru.mcreation.proxwian.helloworld;
+
+/**
+ *
+ * @author Proxwian
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.print("Hello world! I'm here :)");
+    }
+    
+}
